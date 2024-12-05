3d32943dc3fc5a6e13e9057122f035bdbdee97a8
muhammad.salahuddin <muhammad.salahuddin@appinsnap.com>
2024-12-05 12:29:31 +0500

    init commit

--- discretescrollviewlibrary/src/main/java/com/sultanayubi/discretescrollviewlibrary/transform/DiscreteScrollItemTransformer.java
+++
@@ -1,7 +0,0 @@
-package com.sultanayubi.discretescrollviewlibrary.transform;
-
-import android.view.View;
-
-public interface DiscreteScrollItemTransformer {
-    void transformItem(View item, float position);
-}
